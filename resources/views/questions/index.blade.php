f8cf8dbe5145bf33d20b0f50df9da622f619f249
dejan1990 <dejan.simo@hotmail.com>
2019-10-30 17:32:05 +0100

    Add votes, answers and views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -8,21 +8,33 @@
                 <div class="card-header">All Questions</div>
 
                 <div class="card-body">
-                    @foreach($questions as $question)
+                   @foreach ($questions as $question)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status {{ $question->status }}">
+                                    <strong>{{ $question->answers }}</strong> {{ Str::plural('answer', $question->answers) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . Str::plural('view', $question->views) }}
+                                </div>
+                            </div>
                             <div class="media-body">
                                 <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                 <p class="lead">
-                                    Asked by:
+                                    Asked by
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                     <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
-                                <p class="mb-0">{{ Str::limit($question->body, 250) }}</p>
+                                {{ Str::limit($question->body, 250) }}
                             </div>
                         </div>
                         <hr>
-                    @endforeach
-                    <div class="text-center">
+                   @endforeach
+
+                    <div class="mx-auto">
                         {{ $questions->links() }}
                     </div>
                 </div>

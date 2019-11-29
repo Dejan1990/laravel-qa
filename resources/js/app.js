6d37f5783b168fbb96c1dafc93c2e1b6c099d041
dejan1990 <dejan.simo@hotmail.com>
2019-11-29 18:35:35 +0100

    Create vue answer component

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 Vue.component('user-info', require('./components/UserInfo.vue').default);
+Vue.component('answer', require('./components/Answer.vue').default);
 
 const app = new Vue({
     el: '#app',

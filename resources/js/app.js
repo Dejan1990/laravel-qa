ad98a34ed81e575be3bae132c9ea1903d80e574e
dejan1990 <dejan.simo@hotmail.com>
2019-12-06 12:55:08 +0100

    Beautify the flash message and confirm dialog

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vue from 'vue';
+import VueIziToast from 'vue-izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+Vue.use(VueIziToast);
+
 Vue.component('user-info', require('./components/UserInfo.vue').default);
 Vue.component('answer', require('./components/Answer.vue').default);
 

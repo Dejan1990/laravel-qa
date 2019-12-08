14406372009fcafe3fe4ead0006fb4b348f0f698
dejan1990 <dejan.simo@hotmail.com>
2019-12-08 13:56:09 +0100

    Rewrite the authorization policy into javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,12 @@ window.Vue = require('vue');
 import Vue from 'vue';
 import VueIziToast from 'vue-izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import Authorization from './authorization/authorize';
+
 Vue.use(VueIziToast);
+Vue.use(Authorization);
+
+
 
 Vue.component('user-info', require('./components/UserInfo.vue').default);
 Vue.component('answer', require('./components/Answer.vue').default);
